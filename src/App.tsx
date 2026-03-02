d0cb904857ed642a839b7bc4a8178b549a4a82ed
David L <david1.lin@epitech.eu>
2026-03-02 03:08:59 +0100

    feat : final push

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+import AppPresentation from "./pages/AppPresentation";
+import Tutorial from "./pages/Tutorial";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/app" element={<AppPresentation />} />
+          <Route path="/tutorial" element={<Tutorial />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
